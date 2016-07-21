2937f716cb0a2edfd259d05262305d2c293508b2
Mr-Wallet <JordanW@axosoft.com>
2016-07-21 08:53:02 -0700

    commit MySpecialClass

---
+++ MySpecialClass.js
@@ -0,0 +1,19 @@
+const _ = require('lodash');
+
+class MySpecialClass {
+  constructor({options, promise, types}) {
+    this.options = _.clone(options);
+    this.things = _.clone(things);
+    this.types = _.clone(types);
+  }
+
+  mergeOptions(newOptions) {
+    this.options = _.assign(this.options, newOptions);
+  }
+
+  mergeTypes(newTypes) {
+    this.types = _.union(this.types, newTypes);
+  }
+}
+
+module.exports = MySpecialClass;
